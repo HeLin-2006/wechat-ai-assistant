3325ae994cc2120a530703ba3fbc92c49e5ebf86
HeLin <3385357141@qq.com>
2026-08-17 12:07:12 +0800

    init: create Spring Boot project

---
+++ src/main/java/com/example/wechataiassistant/WechatAiAssistantApplication.java
@@ -0,0 +1,13 @@
+package com.example.wechataiassistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WechatAiAssistantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WechatAiAssistantApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/wechataiassistant/WechatAiAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wechataiassistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WechatAiAssistantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
